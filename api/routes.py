3689884ba6a3b2f3c0bda9fa3f2bc578ca0186c3
gabrielCos <chris-19121@hotmail.com>
2025-06-12 17:23:40 -0300

    specifc routes for courses and chapters

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
 import contextlib
+from bson import ObjectId
 from db import db
 
 router = APIRouter()
@@ -44,3 +46,41 @@ def get_courses(sort_by: str = 'date', domain: str = None):
         ).sort(sort_field, sort_order)
     
     return list(courses)
+
+@router.get('/courses/{course_id}')
+def get_course(course_id: str):
+    course = db.courses.find_one(
+        {'_id' : ObjectId(course_id)}, #search for id 
+        {'_id': 0, 'chapters': 0} #show the result whitout the id and chapters fields
+    )
+    # returns error 404 if dont find the course
+    if not course:
+        raise HTTPException(status_code = 404, detail='Course not found')
+    try:
+        course['rating'] = course['rating']['total'] # try to access the rating field
+    except:
+        course['rating'] = 'Not rated yet' #if does not exist, shows a message
+    
+    return course
+
+@router.get('courses/{course_id}/{chapter_id}')
+def get_chapter(course_id: str, chapter_id: str):
+    course = db.courses.find_one(
+        {'_id' : ObjectId(course_id)}, 
+        {'_id': 0, } #excludes the id field of the projection
+    )
+    
+    if not course:
+        raise HTTPException(status_code=404, detail="Course not found")
+    
+    #gets the list of chapters or a empity list
+    chapters = course.get('chapters', [])
+    
+    try:
+        # Try to access the chapter by index converted to integer
+        chapter = chapters[int(chapter_id)]
+    except (ValueError, IndexError) as e:
+        # if the index is not a valid integer, return this message
+        raise HTTPException(status_code=404, detail="Chapter not found") from e
+    
+    return chapter
